a0f077c37df0cd83b9472cde3d8e4d58e4b5f2fa
kusalsandamal2002 <kusal.sandamal@gmail.com>
2025-09-17 12:29:53 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Homepage from "./pages/Homepage.jsx";
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Welcome to My Express App!");
+});
+
+// Example API route
+app.get("/api/departments", (req, res) => {
+  res.json([
+    { id: 1, name: "Computer Science", head: "Dr. Perera" },
+    { id: 2, name: "Electrical Engineering", head: "Dr. Silva" },
+  ]);
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
